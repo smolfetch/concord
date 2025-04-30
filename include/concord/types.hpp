b76ce26e267fe1e9b11182e767e02bd368ec4495
Trim Bresilla <trim.bresilla@gmail.com>
2025-04-30 11:12:50 +0200

    feat: add WGS and ENU coordinate conversions
    - Add conversion functions between WGS and ENU coordinate types

--- include/concord/types.hpp
+++ include/concord/types.hpp
@@ -1,21 +1,37 @@
 #pragma once
 
+#include "wgs_to_enu.hpp"
+
 namespace concord {
+    struct ENU;
+    struct WGS;
+
     struct WGS {
         double lat;
         double lon;
         double alt;
+
+        inline ENU toENU(WGS current, WGS datum);
     };
 
     struct ENU {
-        double xE;
-        double yN;
-        double zU;
-    };
-
-    struct ECEF {
         double x;
         double y;
         double z;
+
+        inline WGS toWGS(ENU current, WGS datum);
     };
+
+    inline ENU WGS::toENU(WGS current, WGS datum) {
+        std::tuple<double, double, double> enu =
+            gps_to_enu(current.lat, current.lon, current.alt, datum.lat, datum.lon, datum.alt);
+        return ENU{std::get<0>(enu), std::get<1>(enu), std::get<2>(enu)};
+    }
+
+    inline WGS ENU::toWGS(ENU current, WGS datum) {
+        std::tuple<double, double, double> wgs =
+            enu_to_gps(current.x, current.y, current.z, datum.lat, datum.lon, datum.alt);
+        return WGS{std::get<0>(wgs), std::get<1>(wgs), std::get<2>(wgs)};
+    }
+
 } // namespace concord
