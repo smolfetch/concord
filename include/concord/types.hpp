da8aeec8a3029705aa856b6b1a5b56bdb45fae32
Trim Bresilla <trim.bresilla@gmail.com>
2025-04-30 11:15:58 +0200

    refactor: introduce and use Datum struct for coordinates
    - Introduce a Datum struct to represent latitude, longitude, and altitude.
    - Modify the toENU and toWGS functions to use the new Datum struct.

--- include/concord/types.hpp
+++ include/concord/types.hpp
@@ -6,12 +6,19 @@ namespace concord {
     struct ENU;
     struct WGS;
 
+    struct Datum {
+        double lat;
+        double lon;
+        double alt;
+    };
+
     struct WGS {
         double lat;
         double lon;
         double alt;
 
-        inline ENU toENU(WGS current, WGS datum);
+        inline ENU toENU(WGS current, Datum datum);
+        inline Datum toDatum(WGS current) { return Datum{current.lat, current.lon, current.alt}; }
     };
 
     struct ENU {
@@ -19,16 +26,16 @@ namespace concord {
         double y;
         double z;
 
-        inline WGS toWGS(ENU current, WGS datum);
+        inline WGS toWGS(ENU current, Datum datum);
     };
 
-    inline ENU WGS::toENU(WGS current, WGS datum) {
+    inline ENU WGS::toENU(WGS current, Datum datum) {
         std::tuple<double, double, double> enu =
             gps_to_enu(current.lat, current.lon, current.alt, datum.lat, datum.lon, datum.alt);
         return ENU{std::get<0>(enu), std::get<1>(enu), std::get<2>(enu)};
     }
 
-    inline WGS ENU::toWGS(ENU current, WGS datum) {
+    inline WGS ENU::toWGS(ENU current, Datum datum) {
         std::tuple<double, double, double> wgs =
             enu_to_gps(current.x, current.y, current.z, datum.lat, datum.lon, datum.alt);
         return WGS{std::get<0>(wgs), std::get<1>(wgs), std::get<2>(wgs)};
