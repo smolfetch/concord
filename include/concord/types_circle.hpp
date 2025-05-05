43b3f4daeb3232de7d4260f39a57c6690a595087
Trim Bresilla <trim.bresilla@gmail.com>
2025-05-06 00:12:34 +0200

    feat: represent circle as polygon
    - Add a method to represent a circle as a polygon

--- include/concord/types_circle.hpp
+++ include/concord/types_circle.hpp
@@ -20,6 +20,20 @@ namespace concord {
             return std::sqrt(std::pow(p.enu.x - center.enu.x, 2) + std::pow(p.enu.y - center.enu.y, 2)) < radius;
         }
 
+        std::vector<Point> as_polygon(int n = 100) const noexcept {
+            std::vector<Point> points;
+            double theta = 2 * M_PI / n;
+            for (int i = 0; i < n; i++) {
+                double x = center.enu.x + radius * std::cos(theta * i);
+                double y = center.enu.y + radius * std::sin(theta * i);
+                Point p;
+                p.enu.x = x;
+                p.enu.y = y;
+                points.push_back(p);
+            }
+            return points;
+        }
+
         double getRadius() const noexcept { return radius; }
         void setRadius(double r) noexcept { radius = r; }
 
